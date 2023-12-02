30e23ff78eef9002f790537ffeb6f52e3640fa35
Luke Dunscombe <dunscombe1@gmail.com>
2023-12-02 15:57:17 -0600

    day 2 part 2

--- src/two/mod.rs
+++ src/two/mod.rs
@@ -4,6 +4,8 @@ pub fn run() {
     let input = include_str!("input.txt");
     let part1 = process(input, false);
     println!("Part1: {}", part1.to_string());
+    let part2 = process(input, true);
+    println!("Part2: {}", part2.to_string());
 }
 
 fn process(input: &str, part2: bool) -> u32 {
@@ -25,7 +27,7 @@ fn process(input: &str, part2: bool) -> u32 {
             for round in game {
                 for cube in round.split(", ") {
                     let (n, color) = cube.split_once(" ").unwrap();
-                    let num = n.parse::<i32>().expect("cube amount must be a number");
+                    let num = n.parse::<u32>().expect("cube amount must be a number");
 
                     if color == "blue" {
                         blue = blue.max(num);
@@ -39,10 +41,15 @@ fn process(input: &str, part2: bool) -> u32 {
                 }
             }
 
-            if red <= 12 && green <= 13 && blue <= 14 {
-                return id;
+            if part2 {
+                return blue * red * green;
             }
-            return 0;
+            else {
+                if red <= 12 && green <= 13 && blue <= 14 {
+                    return id;
+                }
+                return 0;
+            }
         }).sum::<u32>();
 }
 
@@ -59,5 +66,15 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
         assert_eq!("18", process(input, false).to_string());
+    }
+
+    #[test]
+    fn part2() {
+        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        assert_eq!("2286", process(input, true).to_string());
     }
 }
