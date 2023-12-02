e5ed87501c985a38cfc26b0e650a36ca45ef4322
Luke Dunscombe <dunscombe1@gmail.com>
2023-12-02 15:52:28 -0600

    day 2 part 1

---
+++ src/two/mod.rs
@@ -0,0 +1,63 @@
+#![allow(warnings, unused)]
+
+pub fn run() {
+    let input = include_str!("input.txt");
+    let part1 = process(input, false);
+    println!("Part1: {}", part1.to_string());
+}
+
+fn process(input: &str, part2: bool) -> u32 {
+    return input
+        .lines()
+        .map(|line| {
+            let (i, d) = line.split_once(": ").unwrap();
+            let id = i.split(" ").last()
+                .expect("game should have an id")
+                .parse::<u32>()
+                .expect("game id should be a valid digit");
+
+            let game = d.split("; ");
+
+            let mut blue = 0;
+            let mut green = 0;
+            let mut red = 0;
+
+            for round in game {
+                for cube in round.split(", ") {
+                    let (n, color) = cube.split_once(" ").unwrap();
+                    let num = n.parse::<i32>().expect("cube amount must be a number");
+
+                    if color == "blue" {
+                        blue = blue.max(num);
+                    }
+                    if color == "green" {
+                        green = green.max(num);
+                    }
+                    if color == "red" {
+                        red = red.max(num);
+                    }
+                }
+            }
+
+            if red <= 12 && green <= 13 && blue <= 14 {
+                return id;
+            }
+            return 0;
+        }).sum::<u32>();
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        let input = "Game 11: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+        assert_eq!("18", process(input, false).to_string());
+    }
+}
